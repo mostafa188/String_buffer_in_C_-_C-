ab12c7b21b9bb75426460fc9d9e0798391dd0074
Mostafa kamal <110708254+mostafa188@users.noreply.github.com>
2022-10-12 15:31:27 +0600

    cin >> ws ;
    What is cin>> ws in c++?
    Using “ cin >> ws ”: Typing “cin>>ws” after “cin” statement tells the compiler to ignore buffer and also to discard all the whitespaces before the actual content of string or character array.

---
+++ string_buffer_in_C++._3.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	
+    int n;
+    char ch;
+    printf("Enter Number then chracter : \n");
+    cin >> n;
+    string str;
+	
+	  cin >> ws ;
+	  getline(cin,str);
+
+    cout<<n<<" "<<str<<endl;
+    return 0;
+}
